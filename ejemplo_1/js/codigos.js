53d5475e83c01c3e62844319db9e26ce061aa1b7
gustavojuan <gustavojuan@gmail.com>
2014-05-30 22:28:55 +0200

    Dia_1_fin
    clase_11 1:04:31

--- ejemplo_1/js/codigos.js
+++ ejemplo_1/js/codigos.js
@@ -1,8 +1,22 @@
 ;
 //alert("Hola esta funcionando");
+
+function sumar(num1,num2)
+{
+    return num1 + num2;
+}
 function pulsarBotoncito()
 {
-    alert("Has pulsado el botoncito");
+    //alert("Has pulsado el botoncito");
+    var resultado = sumar(47,50);
+    document.getElementById("resultado").innerHTML = resultado;
 }
 
-document.getElementById("parrafito").innerHTML = "CAMBIO DE TEXTO";
+function alCargarElDocumento()
+{
+	var boton = document.getElementById("botoncito");
+    document.getElementById("parrafito").innerHTML = "CAMBIO DE TEXTO";
+	boton.onclick = pulsarBotoncito;
+}
+
+window.onload = alCargarElDocumento;
